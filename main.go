4a4c8e62a914707d93c69a2e73e7a911b591e974
Eduardo Salazar <jesus.sgomez@alumnos.udg.mx>
2020-09-28 23:53:41 -0500

    prueba1

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var num uint64
+	var num1 uint64
+
+	fmt.Scanln(&num)
+	fmt.Scanln(&num1)
+
+	area := (num * num1) / 2
+
+	fmt.Println(area)
 }
